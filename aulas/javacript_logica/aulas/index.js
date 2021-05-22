7d34fb4377b5c9c8958496e4e23acc4d4e9a6fc8
Kamila-Vieira <vkamila54@outlook.com>
2021-05-22 14:12:04 -0300

    add aula javascript logica

--- aulas/javacript_logica/aulas/index.js
+++ aulas/javacript_logica/aulas/index.js
@@ -1,1 +1,84 @@
 // Curto circuito
+/* 
+Valores Falsy
+false
+null / undefined
+'' "" ``
+NaN
+0
+*/
+const file = require('./file.json');
+
+console.log('Kamila' && 'Almeida');
+console.log('Kamila' && '');
+
+function falaOi() {
+  return 'oi';
+}
+let vaiExecutar = 'kaaa';
+console.log(vaiExecutar && falaOi());
+
+console.log('' || 'segundo' || 'terceiro');
+
+console.log(0 || false || null || undefined || 'kamila');
+
+const data = new Date(Date.now());
+
+//console.log(data.toString());
+console.log('Dia', data.getDate());
+console.log('Mês', data.getMonth() + 1);
+console.log('Ano', data.getFullYear());
+console.log('Horas', data.getHours());
+console.log('Minutos', data.getMinutes());
+console.log('Segundos', data.getSeconds());
+console.log('Dia da Semana', data.getDay());
+console.log('Agora', data.toString());
+
+//Desestruturação Array
+
+const array = [1, 2, 3, 4, 5, 6];
+const [um, , tres, ...resto] = array;
+const [quatro, cinco, seis] = resto;
+
+console.log(um, tres);
+console.log(quatro, cinco, seis);
+
+//Tratar e lançar erros
+
+function naoExistoFct(x, y) {
+  if (typeof x !== 'number') {
+    throw new Error('x precisa ser número.');
+  }
+  if (typeof y !== 'number') {
+    throw new Error('y precisa ser número.');
+  }
+  return x + y;
+}
+
+try {
+  naoExisto();
+} catch (error) {
+  console.log('a função naoExisto não existe');
+} finally {
+  console.log(naoExistoFct(1, 2));
+}
+
+function jsonParse(json) {
+  try {
+    console.log(JSON.parse(json));
+  } catch (error) {
+    if (error instanceof SyntaxError) {
+      console.log('o arquivo file.json possui um erro de sintax');
+      //console.error(error.lineNumber);
+      //console.error(error.columnNumber);
+      //console.error(error.message);
+      //console.error(error.name);
+      //console.error(error.fileName);
+      //console.error(error.stack);
+    }
+  } finally {
+    console.log('Stringify antes de parse: ', JSON.parse(JSON.stringify(json)));
+  }
+}
+
+jsonParse(file);
