7fcde498ca8d2f76c461229e7d80d20549b0dfcf
Kamila-Vieira <vkamila54@outlook.com>
2021-05-22 17:18:55 -0300

    Código calculadora

---
+++ aulas/javacript_funcoes/aula/index.js
@@ -0,0 +1,97 @@
+//closures
+
+function retornaOutraFunção(nome) {
+  return function () {
+    return nome;
+  };
+}
+
+const funcaoPrincipal = retornaOutraFunção('kamila');
+const funcaoPrincipal2 = retornaOutraFunção('Vieira');
+
+console.log(funcaoPrincipal.call());
+console.log(funcaoPrincipal2.call());
+
+//callbacks
+
+function rand(min = 1000, max = 3000) {
+  const num = Math.random() * (max - min) + min;
+  return Math.floor(num);
+}
+
+function f1(callback) {
+  setTimeout(function () {
+    console.log('f1');
+    if (callback) callback();
+  }, rand());
+}
+
+function f2(callback) {
+  setTimeout(function () {
+    console.log('f2');
+    if (callback) callback();
+  }, rand());
+}
+
+function f3(callback) {
+  setTimeout(function () {
+    console.log('f3');
+    if (callback) callback();
+  }, rand());
+}
+
+f1(f1Callback);
+
+function f1Callback() {
+  f2(f2Callback);
+}
+
+function f2Callback() {
+  f3(f3Callback);
+}
+
+function f3Callback() {
+  console.log('Olá mundo!');
+}
+
+//factory functions
+
+function criaPessoa(nome, sobrenome, a, p) {
+  return {
+    nome,
+    sobrenome,
+
+    // Getter
+    get nomeCompleto() {
+      return `${this.nome} ${this.sobrenome}`;
+    },
+
+    // Setter
+    set nomeCompleto(valor) {
+      valor = valor.split(' ');
+      this.nome = valor.shift();
+      this.sobrenome = valor.join(' ');
+    },
+
+    fala(assunto = 'falando sobre NADA') {
+      return `${this.nome} está ${assunto}.`;
+    },
+
+    altura: a,
+    peso: p,
+
+    // Getter
+    get imc() {
+      const indice = this.peso / this.altura ** 2;
+      return indice.toFixed(2);
+    },
+  };
+}
+
+const p1 = criaPessoa('Luiz', 'Otávio', 1.8, 80);
+const p2 = criaPessoa('João', 'Otávio', 1.9, 57);
+const p3 = criaPessoa('Junior', 'Otávio', 1.5, 110);
+
+console.log(p1.imc);
+console.log(p2.imc);
+console.log(p3.imc);
---
+++ aulas/javacript_funcoes/exercicios/calculadora/assets/js/main.js
@@ -0,0 +1,70 @@
+function criaCalculadora() {
+  return {
+    display: document.querySelector('.display'),
+    inicia() {
+      this.clickBotoes();
+      this.pressionaBotoes();
+    },
+    pressionaBotoes() {
+      document.addEventListener('keyup', (e) => {
+        e.preventDefault();
+        if (/[0-9-\.*\/\,\+()]/.test(e.key)) this.btnParaDisplay(e.key);
+
+        if (e.keyCode === 13) this.calculaValores();
+
+        if (e.keyCode === 8) this.apagarCaractere();
+
+        if (e.keyCode === 46) this.limpaDisplay();
+      });
+    },
+    clickBotoes() {
+      document.onclick = (e) => {
+        const target = e.target;
+        if (target.classList.contains('btn-num')) {
+          this.btnParaDisplay(target.innerText);
+        }
+        if (target.classList.contains('btn-clear')) {
+          this.limpaDisplay();
+        }
+        if (target.classList.contains('btn-del')) {
+          this.apagarCaractere();
+        }
+        if (target.classList.contains('btn-equal')) {
+          this.calculaValores();
+        }
+      };
+    },
+    btnParaDisplay(valor) {
+      this.display.value += valor;
+    },
+    limpaDisplay() {
+      this.display.value = '';
+    },
+    apagarCaractere() {
+      this.display.value = this.display.value.slice(
+        0,
+        this.display.value.length - 1,
+      );
+    },
+    calculaValores() {
+      let conta = this.display.value.replace('x', '*');
+      try {
+        conta = eval(conta);
+        if (!conta) {
+          alert('Conta Inválida');
+          this.limpaDisplay();
+          return;
+        }
+        this.display.value = conta;
+      } catch (error) {
+        alert('Conta Inválida');
+        this.limpaDisplay();
+        return;
+      }
+    },
+  };
+}
+
+const calculadora = criaCalculadora();
+
+calculadora.inicia();
