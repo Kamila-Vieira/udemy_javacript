a5c1f1ae4a7c3539831c75e4d3bd500419b1566c
Kamila-Vieira <vkamila54@outlook.com>
2021-07-07 19:11:04 -0300

    Atualização curso deploy

---
+++ deploy_aplicacao/agenda_de_contatos/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path"); // CommonJS
+
+module.exports = {
+  mode: "production", // Ambiente de desenvolvimento development/production
+  entry: "./frontend/main.js",
+  output: {
+    path: path.resolve(__dirname, "public", "assets", "js"), // onde ficará o arquivo transpilado
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/, // pasta que não será transpilada
+        test: /\.js$/, // tipos de arquivos que serão analisados e transpilados
+        use: {
+          // o que o transpilador irá usar
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/env"],
+          },
+        },
+      },
+      // {
+      //   test: /\.css$/,
+      //   use: ["style-loader", "css-loader"],
+      // },
+    ],
+  },
+  devtool: "source-map", // Mapei erro no arquivo bundle.js
+};
