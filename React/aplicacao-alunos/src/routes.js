45b7fbb1c39b5b99534bd3f282eb8c17945b2baf
Kamila-Vieira <vkamila54@outlook.com>
2022-06-18 15:20:38 -0300

    Ajuste de rotas

--- React/aplicacao-alunos/src/routes.js
+++ React/aplicacao-alunos/src/routes.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Route, Routes as BrowserRoutes } from 'react-router-dom';
 
-import Logout from '@pages/Logout';
-import Aluno from '@pages/Aluno';
-import Alunos from '@pages/Alunos';
-import Fotos from '@pages/Fotos';
-import Register from '@pages/Register';
-import Login from '@pages/Login';
-import Page404 from '@pages/Page404';
-import PrivateRoute from './PrivateRoute';
+import Aluno from './pages/Aluno';
+import Alunos from './pages/Alunos';
+import Fotos from './pages/Fotos';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Logout from './pages/Logout';
+import Page404 from './pages/Page404';
+import PrivateRoute from './pages/PrivateRoute';
 
 export default function Routes() {
   return (
