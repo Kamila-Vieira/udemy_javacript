5792516a123fb983728e9f4d5a664e11c0d929a9
Kamila-Vieira <vkamila54@outlook.com>
2022-06-18 15:26:23 -0300

    Teste componente login para deploy

--- React/aplicacao-alunos/src/routes.js
+++ React/aplicacao-alunos/src/routes.js
@@ -5,7 +5,7 @@ import Aluno from './pages/Aluno';
 import Alunos from './pages/Alunos';
 import Fotos from './pages/Fotos';
 import Register from './pages/Register';
-import Login from './pages/Login';
+import LoginContent from './pages/Login';
 import Logout from './pages/Logout';
 import Page404 from './pages/Page404';
 import PrivateRoute from './pages/PrivateRoute';
@@ -16,7 +16,7 @@ export default function Routes() {
       <Route path="*" element={<Page404 />} />
       <Route path="/" element={<Alunos />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<Login />} />
+      <Route path="/login" element={<LoginContent />} />
       <Route path="/logout" element={<Logout />} />
       <Route element={<PrivateRoute />}>
         <Route path="/aluno/:id/edit" element={<Aluno />} />
