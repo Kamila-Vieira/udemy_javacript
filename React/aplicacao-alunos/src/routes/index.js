dbb4bac7a8eb8fbce4bae6c0f1db3ccf16dc9aa2
Kamila-Vieira <vkamila54@outlook.com>
2022-06-18 15:12:21 -0300

    Testando rotas

--- React/aplicacao-alunos/src/routes/index.js
+++ React/aplicacao-alunos/src/routes/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Route, Routes as BrowserRoutes } from 'react-router-dom';
 
-import Aluno from '../pages/Aluno';
-import Alunos from '../pages/Alunos';
-import Fotos from '../pages/Fotos';
-import Register from '../pages/Register';
-import Login from '../pages/Login';
-import Logout from '../pages/Logout';
-import Page404 from '../pages/Page404';
+import Logout from '@pages/Logout';
+import Aluno from '@pages/Aluno';
+import Alunos from '@pages/Alunos';
+import Fotos from '@pages/Fotos';
+import Register from '@pages/Register';
+import Login from '@pages/Login';
+import Page404 from '@pages/Page404';
 import PrivateRoute from './PrivateRoute';
 
 export default function Routes() {
